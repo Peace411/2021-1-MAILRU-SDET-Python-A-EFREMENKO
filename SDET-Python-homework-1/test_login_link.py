b5786ac7b37de656e96bca9c549b787e6262eea2
Peace411 <72517021+Peace411@users.noreply.github.com>
2021-03-23 22:36:06 +0300

    delete

--- SDET-Python-homework-1/test_login_link.py
+++
@@ -1,9 +0,0 @@
-from pages.login_page import LoginPage
-import pytest
-@pytest.mark.ui
-def test_login_link(browser):
-    link = "https://target.my.com/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.click_to_login_button()
-    page.authorization()
