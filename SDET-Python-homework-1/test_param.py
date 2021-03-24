fa2612e4249366415290018fb06c6c93b832bfa5
Peace411 <72517021+Peace411@users.noreply.github.com>
2021-03-25 00:02:17 +0300

    Update test_param.py

--- SDET-Python-homework-1/test_param.py
+++ SDET-Python-homework-1/test_param.py
@@ -1,19 +1,21 @@
 from pages.login_page import LoginPage
-from pages.param_page import ParamPage
-from pages.locators import ParamPageLocators
+from pages.audiences_page import AudiencesPage
+from pages.pro_page import ProPage
+from pages.locators import ProPageLocators
+from pages.locators import AudiencesPageLocators
 import pytest
 import time
 
-@pytest.mark.parametrize("sel", [ParamPageLocators.AUDIENCES, ParamPageLocators.PRO])
+@pytest.mark.parametrize("locator,expected_page", [(AudiencesPageLocators.AUDIENCES_BUTTON,AudiencesPage), (ProPageLocators.PRO_BUTTON,ProPage)])
 @pytest.mark.ui
-def test_param(browser,sel):
+def test_param(browser,locator,expected_page):
     link = "https://target.my.com/"
     page = LoginPage(browser, link)
     page.open()
     page.click_to_login_button()
     page.authorization()
-    page = ParamPage(browser, link)
-    page.go_to_page(sel)
+    page = expected_page(browser, link)
+    page.go_to_page()
 
 
 
