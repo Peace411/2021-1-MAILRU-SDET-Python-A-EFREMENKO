78985a59151f3eda79c9a5b832eea34369278fcc
Peace411 <72517021+Peace411@users.noreply.github.com>
2021-03-25 15:58:26 +0300

    Update locators.py

--- SDET-Python-homework-1/pages/locators.py
+++ SDET-Python-homework-1/pages/locators.py
@@ -2,28 +2,28 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, ".responseHead-module-button-1BMAy4")
-    INPUT_EMAIL =(By.CSS_SELECTOR, ".authForm-module-inputWrap-38SKYw> input")
+    LOGIN_LINK = (By.XPATH, "//*[normalize-space() = 'Войти']")
+    INPUT_EMAIL =(By.CSS_SELECTOR, "[name ='email']")
     INPUT_PASSWORD =(By.CSS_SELECTOR, '[type="password"]')
 
 
 class LogoutPageLocators():
-    RIGHT_BUTTON =(By.CSS_SELECTOR, '.right-module-rightWrap-3lL6mf')
-    LOGOUT_BUTTON= (By.XPATH,"//a[@href='/logout']")
+    RIGHT_BUTTON =(By.XPATH, '//div[text()="Баланс: "]')
+    LOGOUT_BUTTON= (By.XPATH, "//a[@href='/logout']")
 
 
 class EditProfilePageLocators():
-    PROFILE_BUTTON= (By.CSS_SELECTOR, '.center-module-profile-BHql9z')
-    FIO_INPUT=(By.CSS_SELECTOR, '.js-contacts-field-name .input__wrap>input')
-    PHONE_NUBER=(By.CSS_SELECTOR, '.js-contacts-field-phone input')
+    PROFILE_BUTTON= (By.XPATH, "//a[contains(@href, '/profile')]")
+    FIO_INPUT=(By.CSS_SELECTOR, '.js-contacts-field-name input')
+    PHONE_NUMBER=(By.CSS_SELECTOR, '.js-contacts-field-phone input')
     INPUT_EMAIL=(By.CSS_SELECTOR, ".js-additional-emails input")
     SUMBIT_BUTTON=(By.CSS_SELECTOR, ".button_submit")
     SUCCESS_MESSAGE= (By.CSS_SELECTOR, ".js-group-form-success-bg")
 
 
 class AudiencesPageLocators():
-    AUDIENCES_BUTTON = (By.XPATH,"//a[contains(@href, 'segments')]")
+    AUDIENCES_BUTTON = (By.XPATH, "//a[contains(@href, 'segments')]")
 
 
 class ProPageLocators():
-    PRO_BUTTON =(By.XPATH,"//a[contains(@href, 'pro')]")
+    PRO_BUTTON =(By.XPATH, "//a[contains(@href, 'pro')]")
